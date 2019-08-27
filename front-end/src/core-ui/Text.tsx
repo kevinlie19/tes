246c8cbe76b5af494c772b7749975c45d064b161
ines <me@ines.click>
2019-08-27 15:05:07 +0700

    text almost done

--- front-end/src/core-ui/Text.tsx
+++ front-end/src/core-ui/Text.tsx
@@ -1,111 +1,78 @@
 import React, { ReactNode } from 'react';
 
 import {
- TextProps,
- StyleSheet,
- Text as TextComponent,
- TouchableOpacity,
- GestureResponderEvent,
+  TextProps,
+  StyleSheet,
+  Text as TextComponent,
 } from 'react-native';
 
-//import { PRIMARY_COLOR, WHITE, CUSTOM_BLACK, GREY } from '../constants/color';
+import { CUSTOM_RED, CUSTOM_BROWN, CUSTOM_YELLOW, CUSTOM_GREEN, BLACK, WHITE, GREY, DARK_GREY, LIGHT_GREY } from '../constants/color';
+import { XXSMALL, XSMALL, SMALL, MEDIUM, MLARGE, LARGE, XLARGE } from '../constants/fontSize';
 
 type Props = TextProps & {
- children: ReactNode;
- definedStyle?:
- | 'headTitle'
- | 'heading'
- | 'subheading'
- | 'regularHome'
- | 'regularAuth'
- | 'small';
- weight?: 'regular' | 'bold' | 'light';
- size?: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge';
- color?: string;
- onPress?: (event: GestureResponderEvent) => void;
+  children?: ReactNode;
+  definedStyle?:
+  | 'headTitle'
+  | 'heading'
+  | 'title'
+  | 'status';
+  weight?: 'regular' | 'bold' | 'light';
+  color?: string;
+  text: string;
+  onPress?: () => void;
 };
 
-const FONT_SIZE = {
- xsmall: 8,
- small: 10,
- medium: 14,
- large: 18,
- xlarge: 24,
-};
 
 const FONT_WEIGHT: any = {
- regular: 'normal',
- bold: 'bold',
+  regular: 'normal',
+  bold: 'bold',
 };
 
 const FONT_COLOR = '#323232';
 
 export default function Text(props: Props) {
- let {
-  children,
-  definedStyle,
-  style,
-  onPress,
-  color,
-  size,
-  weight,
-  ...other
- } = props;
+  let {
+    children,
+    definedStyle,
+    style,
+    onPress,
+    color,
+    weight,
+    text,
+    ...other
+  } = props;
 
- let fontWeight = FONT_WEIGHT[weight || 'regular'];
- let fontSize = FONT_SIZE[size || 'small'];
- let textColor = color || FONT_COLOR;
+  let fontWeight = FONT_WEIGHT[weight || 'regular'];
+  let textColor = color || FONT_COLOR;
 
- let textStyle = { fontWeight, fontSize, color: textColor };
+  let textStyle = { fontWeight, color: textColor };
 
- let mixedStyles = [
-  textStyle,
-  definedStyle && styles[definedStyle],
-  style && style,
- ];
+  return (
+    <TextComponent>{text}</TextComponent>
+  )
 
- return onPress ? (
-  <TouchableOpacity onPress={onPress}>
-   <TextComponent style={mixedStyles} {...other}>
-    {children}
-   </TextComponent>
-  </TouchableOpacity>
- ) : (
-   <TextComponent style={mixedStyles} {...other}>
-    {children}
-   </TextComponent>
-  );
 }
 
 const styles = StyleSheet.create({
- headTitle: {
-  fontSize: 24,
-  color: ,
-  fontWeight: 'bold',
- },
- heading: {
-  fontSize: 18,
-  color: ,
-  fontWeight: 'bold',
- },
- subheading: {
-  fontSize: 14,
-  color: ,
-  fontWeight: 'regular',
- },
- small: {
-  fontSize: 10,
-  color: ,
-  fontWeight: 'regular',
- },
-});
+  headTitle: {
+    fontSize: LARGE,
+    color: WHITE,
+    fontWeight: 'bold',
+  },
+  heading: {
+    fontSize: LARGE,
+    color: BLACK,
+    fontWeight: 'bold',
+  },
+  title: {
+    fontSize: MEDIUM,
+    color: BLACK,
+    fontWeight: 'bold',
+  },
+  status: {
+    fontSize: XSMALL,
+    color: GREY || LIGHT_GREY || CUSTOM_RED || CUSTOM_GREEN,
+    fontWeight: 'bold',
+  },
 
-//yellow: ffb900
-//brown:d3793d
-//green:3db67a
-//grey:818181
-//lightgrey:cbcbcb
-//SlightGrey:eaeaea
-//red:dd3945
-//white:ffffff
-//black:323232
+});
