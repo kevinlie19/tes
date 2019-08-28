ffee899e18e01150b0ffd1bffb7cad87a302d85b
Kevin Lie <Me@Kevin.local>
2019-08-28 10:23:09 +0700

    Fixing Core-UI Image & Text

--- front-end/src/core-ui/Image.tsx
+++ front-end/src/core-ui/Image.tsx
@@ -4,57 +4,52 @@ import { StyleSheet, StyleProp, ViewStyle } from 'react-native';
 import { Image as Picture } from 'react-native-elements';
 
 type Props = {
- newImageStyle?: StyleProp<ViewStyle>;
- src?: string;
- type?: 'event' | 'banner' | 'square' | 'xLarge';
- resizeMode?: 'center' | 'contain' | 'stretch' | 'cover' | 'repeat';
-};
-
-const IMAGE = {
- placeholderImage: require('../../assets/placeholder.jpg'),
+  newImageStyle?: StyleProp<ViewStyle>;
+  src?: string;
+  type?: 'event' | 'banner' | 'square' | 'xLarge';
+  resizeMode?: 'center' | 'contain' | 'stretch' | 'cover' | 'repeat';
 };
 
 export default function Image(props: Props) {
- let { src, type, resizeMode, newImageStyle, ...other } = props;
+  let { src, type, resizeMode, newImageStyle, ...other } = props;
 
- return !src ? (
-  <Picture
-   source={IMAGE['placeholderImage']}
-   style={styles[type || 'square']}
-   containerStyle={newImageStyle}
-   resizeMode={resizeMode ? resizeMode : 'contain'}
-   {...other}
-  />
- ) : (
-   <Picture
-    source={{ uri: src }}
-    style={styles[type || 'square']}
-    containerStyle={newImageStyle}
-    resizeMode={resizeMode ? resizeMode : 'contain'}
-    {...other}
-   />
+  return !src ? (
+    <Picture
+      source={require('../../assets/images/placeholder.jpg')}
+      style={styles[type || 'square']}
+      containerStyle={newImageStyle}
+      resizeMode={resizeMode}
+      {...other}
+    />
+  ) : (
+    <Picture
+      source={{ uri: src }}
+      style={styles[type || 'square']}
+      containerStyle={newImageStyle}
+      resizeMode={resizeMode}
+      {...other}
+    />
   );
 }
 
 const styles = StyleSheet.create({
- square: {
-  width: 54,
-  height: 54,
-  alignSelf: 'center',
-  borderRadius: 4,
- },
- banner: {
-  width: 328,
-  height: 100,
- },
- event: {
-  width: 120,
-  height: 56,
-  borderRadius: 4,
- },
- xLarge: {
-  width: 360,
-  height: 240,
- },
-
+  square: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    alignSelf: 'center',
+  },
+  event: {
+    width: 120,
+    height: 56,
+    borderRadius: 4,
+  },
+  banner: {
+    width: 328,
+    height: 100,
+  },
+  xLarge: {
+    width: 360,
+    height: 240,
+  },
 });
