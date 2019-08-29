490e51b75e2b8bc04ae3511df0a23287a646591a
Kevin Lie <Me@capricco.lan>
2019-08-29 18:18:31 +0700

    Add Core-UI Avatar

--- front-end/src/core-ui/index.ts
+++ front-end/src/core-ui/index.ts
@@ -2,5 +2,6 @@ import Icon from './Icon';
 import Text from './Text';
 import Image from './Image';
 import Button from './Button';
+import Avatar from './Avatar';
 
-export { Icon, Text, Image, Button };
+export { Icon, Text, Image, Button, Avatar };
