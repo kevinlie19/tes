7d2ac536fe5d792322dd0cf08e78606ea52dcfcc
Kevin Lie <Me@Kevin.local>
2019-09-03 17:16:00 +0700

    MinorFix Navigation

--- front-end/src/scenes/WelcomeScenes.tsx
+++ front-end/src/scenes/WelcomeScenes.tsx
@@ -7,31 +7,32 @@ import { LIGHT_BLACK } from '../constants/color';
 type Props = {};
 
 export default class WelcomeScene extends Component<Props> {
- render() {
-  return (
-   <View style={styles.imageBackground}>
-    <View style={styles.imageContainer}>
-     <Icon name="logo" isActive={false} customStyle={styles.logo} />
-    </View>
-   </View>
-  );
- }
+  render() {
+    return (
+      <View style={styles.imageBackground}>
+        <View style={styles.imageContainer}>
+          <Icon name="logo" isActive={false} customStyle={styles.logo} />
+        </View>
+        <View></View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
- imageContainer: {
-  flex: 1,
-  justifyContent: 'center',
+  imageContainer: {
+    flex: 1,
+    justifyContent: 'center',
 
-  alignItems: 'center',
- },
- imageBackground: {
-  width: '100%',
-  height: '100%',
-  backgroundColor: LIGHT_BLACK,
- },
- logo: {
-  width: 152,
-  height: 160,
- },
+    alignItems: 'center',
+  },
+  imageBackground: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: LIGHT_BLACK,
+  },
+  logo: {
+    width: 152,
+    height: 160,
+  },
 });
