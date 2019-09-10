39638865c5f3c8b1d36c8b018e032718f0e660d0
Kevin Lie <48467219+kevinlie19@users.noreply.github.com>
2019-09-10 14:41:20 +0700

    Add HomeScene & MinorFix everything (#17) (#18)

--- front-end/src/components/EventList.tsx
+++ front-end/src/components/EventList.tsx
@@ -16,22 +16,44 @@ export default function EventList(props: Props) {
   let { category, title, date, price, onPress } = props;
 
   return (
-    <TouchableOpacity onPress={onPress}>
+    <TouchableOpacity onPress={onPress} style={styles.eventListContainer}>
       <Image type="event" />
-      <Text text={category} type="xxsmall" />
-      <Text text={title} type="small" newTextStyle={styles.titleEvent} />
-      <Text text={date} type="xsmall" />
+      <Text
+        text={category}
+        type="xxsmall"
+        newTextStyle={styles.categoryEvent}
+      />
+      <Text
+        text={title}
+        type="small"
+        newTextStyle={styles.titleEvent}
+        ellipsizeMode="tail"
+        numberOfLines={1}
+      />
+      <Text text={date} type="xsmall" newTextStyle={styles.dateEvent} />
       <Text text={price} type="xsmall" newTextStyle={styles.priceEvent} />
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
+  eventListContainer: {
+    maxWidth: 120,
+    marginRight: 16,
+  },
+  categoryEvent: {
+    marginTop: 8,
+  },
   titleEvent: {
     fontWeight: 'bold',
+    marginTop: 4,
+  },
+  dateEvent: {
+    marginTop: 4,
   },
   priceEvent: {
     color: CUSTOM_YELLOW,
     fontWeight: 'bold',
+    marginTop: 4,
   },
 });
