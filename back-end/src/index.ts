c022141e7b4d15aa17db8a5164b640cc98efb4d8
Kevin Lie <48467219+kevinlie19@users.noreply.github.com>
2019-09-16 09:38:13 +0700

    New setup for Back-End (#24)

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,48 +1,49 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import { QueryResult } from 'pg';
 
-let app = express();
+import { SERVER_OK, PORT } from './constants';
+import apiRouter from './routers';
+import { getDB } from './db';
+import { cloudinaryConfig } from './cloudinarySetup';
 
-let db = {
-  tasks: [
-    {
-      id: 1,
-      name: 'masak nasi',
-    },
-    {
-      id: 2,
-      name: 'masak air',
-    },
-  ],
-};
+const app: express.Application = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+async function serverSetup() {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('success');
-});
+  app.locals.db = await getDB();
+  await app.locals.db.query(
+    'create table users(ID SERIAL PRIMARY KEY, email varchar(80) UNIQUE, first_name varchar(50), last_name varchar(50), password text, avatar varchar(50), membership varchar(50), gender varchar(50))',
+    (error: Error, results: QueryResult) => {},
+  );
 
-app.get('/task/', (req, res) => {
-  res.json(db.tasks);
-});
+  await app.locals.db.query(
+    'create table events(ID SERIAL PRIMARY KEY, event_name varchar(50) UNIQUE, category varchar(50), place varchar(50), price varchar(50), description varchar(100), available_seat int, image varchar(50))',
+    (error: Error, results: QueryResult) => {},
+  );
 
-app.get('/task/:id', (req, res) => {
-  let id = req.params.id;
-  for (let i = 0; i < db.tasks.length; i++) {
-    if (db.tasks[i].id === Number(id)) {
-      res.json(db.tasks[i]);
-    }
-  }
-});
+  await app.locals.db.query(
+    'create table user_event(ID_user SERIAL REFERENCES users(ID), ID_event SERIAL REFERENCES events(ID), PRIMARY KEY(ID_user, ID_event))',
+    (error: Error, results: QueryResult) => {},
+  );
 
-app.post('/task', (req, res) => {
-  let body = req.body;
+  app.use('*', cloudinaryConfig);
 
-  res.json(body);
-  db.tasks.push(body);
-});
+  app.get('/', (req: Request, res: Response) => {
+    res.status(SERVER_OK);
+    res.send('Accessing Back-end Success.');
+  });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
-});
+  app.use('/api', apiRouter);
+
+  app.on('listening', function() {
+    console.log('server is running');
+  });
+  app.listen(PORT, () => {
+    console.log(`App is listening on http://127.0.0.1:${PORT}`);
+  });
+}
+
+serverSetup();
