dbda3ce3d38fe6f5ecafd955a3d812f83dfdd8c6
Kevin Lie <kevinlie19@gmail.com>
2019-12-09 10:23:38 +0700

    Initial Heroku

--- back-end/src/db.ts
+++ back-end/src/db.ts
@@ -1,20 +1,43 @@
-import { Client } from 'pg';
+import { Pool } from 'pg';
 
-import { DB_PORT, USER, HOST, DATABASE_NAME, PASSWORD } from './constants';
+import {
+  DB_PORT,
+  USER,
+  HOST,
+  DATABASE_NAME,
+  PASSWORD,
+  DATABASE_URL,
+} from './constants';
 
-let db: Promise<Client> | undefined;
+let db: Promise<Pool> | undefined;
+
+const connectionString = DATABASE_URL;
+
+let localConfig = {
+  user: USER,
+  host: HOST,
+  database: DATABASE_NAME,
+  password: PASSWORD,
+  port: DB_PORT,
+};
 
 async function connect() {
-  let client = new Client({
-    user: USER,
-    host: HOST,
-    database: DATABASE_NAME,
-    password: PASSWORD,
-    port: DB_PORT,
-  });
+  let client = new Pool(localConfig);
   try {
+    let client = new Pool(
+      // comment this if you want to use heroku database <------
+      // localConfig,
+
+      //comment this if you want to use localhost database <------
+      {
+        connectionString,
+        ssl: true,
+      },
+    );
     await client.connect();
     console.log('Database connected!');
+
+    return client;
   } catch (e) {
     console.log(e);
   }
